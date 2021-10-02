afc58e7df7f453d9c480c192b345b12467540ede
raphael-fritz <raphael-fritz@users.noreply.github.com>
2021-10-02 18:42:06 +0200

    add console output

--- main.py
+++ main.py
@@ -24,15 +24,20 @@ import spotify_playlist_tracker as spt
 def main():
 
     print(datetime.datetime.fromtimestamp(time.time()))
-    
+    print("Authenticating with Spotify...")
     # authenticate with spotify
     scope = "playlist-modify-public"
     spotify = spt.spotify_authentication(
         config.client_id, config.client_secrect, config.redirect_uri, scope=scope)    
     user_list = spt.get_spotify_users(spotify)
-    
+
+    print("Creating/checking directory structure...")
     spt.create_dir_structure(user_list)
-    spt.update_dir_structure(user_list)
+
+    print("Updating directory structure...")
+    spt.update_dir_structure(spotify, user_list)
+
+    print("Finished!")
 
 
 if __name__ == "__main__":
