c0a386463f049ee3746588c279b174593279f95d
raphael-fritz <raphael-fritz@users.noreply.github.com>
2021-12-22 14:39:39 +0100

    refactoring

--- spotify_playlist_tracker.py
+++ spotify_playlist_tracker.py
@@ -1,17 +1,16 @@
 import spotipy
-import re
 from spotify_user import Spotify_Playlist, Spotify_User
 from pathlib import Path
 from datetime import datetime
 
 
-def spotify_authentication(client_id, client_secrect, redirect_uri, scope, openBrowser):
+def spotify_authentication(client_id:str, client_secrect:str, redirect_uri:str, scope:str, openBrowser:bool) -> spotipy.client.Spotify:
     auth_manager = spotipy.oauth2.SpotifyOAuth(
         client_id=client_id, client_secret=client_secrect, redirect_uri=redirect_uri, scope=scope, open_browser=openBrowser)
     return spotipy.client.Spotify(auth_manager=auth_manager)
 
 
-def get_spotify_users(spotify: spotipy.client.Spotify):
+def get_spotify_users(spotify: spotipy.client.Spotify) -> "list[Spotify_User]":
     usernames = []
     with open("usernames.txt") as username_list:
         for username in username_list:
@@ -29,79 +28,51 @@ def get_spotify_users(spotify: spotipy.client.Spotify):
     return user_list
 
 
-def get_playlists(user: Spotify_User):
+"""def get_playlists(user: Spotify_User) -> "list[str]":
     playlists = []
     for pl in user.playlists:
         playlists.append(pl["name"])
-    return playlists
+    return playlists"""
 
 
-def get_tracks(spotify: spotipy.client.Spotify, uri: str):
+def get_tracks(spotify: spotipy.client.Spotify, uri: str) -> "list[str]":
     tracks = []
     for track in Spotify_Playlist(spotify, uri).tracks:
         tracks.append(str(track["artists"][0]["name"] + "-" + track["name"]))
     return tracks
 
 
-def rpl_bad_chars(string: str):
-    bad_chars = {"ä": "ae", "ö": "oe", "ü": "ue",
-                 "Ä": "AE", "Ö": "OE", "Ü": "UE", "ß": "ss", ".": "_"}
-    rpl_pattern = r"(\s)|([^\w\-_\.\ ])"
-
-    for char in bad_chars:
-        string = string.replace(char, bad_chars[char])
-    return re.sub(rpl_pattern, "_", string)
-
-
-def get_path_strings(username: str, playlists: list):
-    usr_str = "data/" + rpl_bad_chars(username)
-    pl_str = usr_str + "/_playlists.txt"
-    pl_changes_str = usr_str + "/_playlists_changes.txt"
-    songs_list = []
-    songs_changes_list = []
-
-    for playlist in playlists:
-        playlist = rpl_bad_chars(playlist)
-        songs_list.append(str(usr_str + "/" + playlist + "_songs.txt"))
-        songs_changes_list.append(str(usr_str + "/" + playlist + "_songs_changes.txt"))
-
-    return usr_str, pl_str, pl_changes_str, songs_list, songs_changes_list
-
-
-def get_diff(list1: list, list2: list):
+def get_diff(list1: list, list2: list) -> tuple:
     diff_p = list(set(set(list1)-set(list2)))
     diff_n = list(set(set(list2)-set(list1)))
     return (diff_p, diff_n)
 
 
-def check_dir(path: str):
+def check_dir(path: str) -> None:
     if not Path(path).is_dir():
         print(path + " doesn't exist and will be created")
         Path(path).mkdir()
 
 
-def check_file(path: str):
+def check_file(path: str) -> None:
     if not Path(path).is_file():
         print(path + " doesn't exist and will be created")
         Path(path).touch()
 
 
-def create_dir_structure(user_list):
+def create_dir_structure(user_list:"list[Spotify_User]") -> None:
+    check_dir("data")
     for user in user_list:
-        usr_str, pl_str, pl_changes_str, songs_list, songs_changes_list = get_path_strings(
-            user.name, get_playlists(user))
-
-        check_dir("data")
-        check_dir(usr_str)
-        check_file(pl_str)
-        check_file(pl_changes_str)
-
-        for i in range(len(songs_list)):
-            check_file(songs_list[i])
-            check_file(songs_changes_list[i])
+        check_dir(user.user_path)
+        check_file(user.pl_path)
+        check_file(user.pl_changes_path)
+        for path in user.song_path_list:
+            check_file(path)
+        for path in user.song_changes_path_list:
+            check_file(path)
 
 
-def read_base_file(path: str):
+def read_base_file(path: str) -> "list[str]":
     with open(path, "r", encoding="utf-8") as file:
         lines = []
         for line in file.readlines():
@@ -109,15 +80,15 @@ def read_base_file(path: str):
         return lines
 
 
-def write_base_file(path: str, base_list: list):
+def write_base_file(path: str, base_list: list) -> None:
     with open(path, "w", encoding="utf-8") as file:
         for entry in base_list:
             file.write(str(entry + "\n"))
 
 
-def write_diff_file(path: str, header: str, diff: "tuple[list, list]"):
+def write_diff_file(path: str, header: str, diff_list: "tuple[list, list]") -> None:
     with open(path, "a", encoding="utf-8") as file:
-        (diff_p, diff_n) = diff
+        (diff_p, diff_n) = diff_list
         print(header + ":", end="")
         if diff_p or diff_n:
             file.write(
@@ -132,32 +103,29 @@ def write_diff_file(path: str, header: str, diff: "tuple[list, list]"):
             print(" x no changes")
 
 
-def update_dir_structure(spotify, user_list: "list[Spotify_User]"):
+def update_dir_structure(spotify:spotipy.client.Spotify, user_list: "list[Spotify_User]") -> None:
     for user in user_list:
-        pl_current = get_playlists(user)
-        pl_str, pl_changes_str = get_path_strings(
-            user.name, pl_current)[1:3]
-        pl_baseline = read_base_file(pl_str)
+        pl_current = user.playlist_names
+        pl_baseline = read_base_file(user.pl_path)
+
         (diff_p, diff_n) = get_diff(pl_current, pl_baseline)
+            
 
-        write_base_file(pl_str, pl_current)
-        write_diff_file(pl_changes_str, user.name, (diff_p, diff_n))
+        write_base_file(user.pl_path, pl_current)
+        write_diff_file(user.pl_changes_path, user.name, (diff_p, diff_n))
 
-        i = 0
-        songs_list, songs_changes_list = get_path_strings(
-            user.name, get_playlists(user))[3:5]
-        for playlist in user.playlists:
+        for i in range(len(user.playlists)):
             try:
-                tracks_current = get_tracks(spotify, playlist["uri"])
-                tracks_baseline = read_base_file(songs_list[i])
+                tracks_current = get_tracks(spotify, user.playlists[i]["uri"])
+                tracks_baseline = read_base_file(user.song_path_list[i])
                 (diff_p, diff_n) = get_diff(tracks_current, tracks_baseline)
 
-                write_base_file(songs_list[i], tracks_current)
-                write_diff_file(songs_changes_list[i], str(
-                    user.name + ": " + playlist["name"]), (diff_p, diff_n))
+                write_base_file(user.song_path_list[i], tracks_current)
+                write_diff_file(user.song_changes_path_list[i], str(
+                    user.name + ": " + user.playlists[i]["name"]), (diff_p, diff_n))
                 i += 1
-            except Exception:
-                print(user.name + ": " + playlist["name"] + ": ")
+            except ValueError:
+                print(user.name + ": " + user.playlists[i]["name"] + ": ValueError")
                 i += 1
                 pass
         print()
--- spotify_user.py
+++ spotify_user.py
@@ -1,4 +1,14 @@
 from spotipy import SpotifyException
+from re import sub
+
+
+def rpl_bad_chars(string: str) -> str:
+    bad_chars = {"ä": "ae", "ö": "oe", "ü": "ue",
+                 "Ä": "AE", "Ö": "OE", "Ü": "UE", "ß": "ss", ".": "_"}
+
+    for char in bad_chars:
+        string = string.replace(char, bad_chars[char])
+    return sub(r"(\s)|([^\w\-_\.\ ])", "_", string)
 
 
 class Spotify_Playlist:
@@ -8,7 +18,7 @@ class Spotify_Playlist:
         self.uri = uri
         self.tracks = self.get_tracks()
         if not len(self.tracks):
-            raise Exception
+            raise ValueError
 
 
     def get_tracks(self):
@@ -37,6 +47,20 @@ class Spotify_User:
         self.name = user_name
         self.id = user_id
         self.playlists = self.get_playlists()
+        self.playlist_names = self.get_playlist_names()
+        self.user_path = str("data/" + rpl_bad_chars(self.name))
+        self.pl_path = str(self.user_path + "/_playlists.txt")
+        self.pl_changes_path = str(self.user_path + "/_playlists_changes.txt")
+        self.song_path_list, self.song_changes_path_list = self.get_song_paths()
+        
+    def get_song_paths(self) -> "tuple[list[str], list[str]]":
+        songs_list = []
+        songs_changes_list = []
+        for playlist in self.playlists:
+            playlist = rpl_bad_chars(playlist["name"])
+            songs_list.append(str(self.user_path + "/" + playlist + "_songs.txt"))
+            songs_changes_list.append(str(self.user_path + "/" + playlist + "_songs_changes.txt"))
+        return songs_list, songs_changes_list
 
     def get_playlists(self):
         user_playlists = []
@@ -49,3 +73,9 @@ class Spotify_User:
             user_playlists.append(playlist)
 
         return user_playlists
+
+    def get_playlist_names(self):
+        playlist_names = []
+        for pl in self.playlists:
+            playlist_names.append(pl["name"])
+        return playlist_names
