886a647f7de503851a33f88145e7a66d55042c0a
raphael-fritz <raphael-fritz@users.noreply.github.com>
2021-12-28 18:14:07 +0100

    improve search function

--- spotify_playlist_tracker.py
+++ spotify_playlist_tracker.py
@@ -123,96 +123,73 @@ def update_user(user: Spotify_User):
         file.write(str(json.dumps(data)+"\n"))
     return data
 
-
-def read_user_events(user: Spotify_User):
+def load_eventstream():
     with open(PATH, "r", encoding=ENC) as file:
-        events = []
-        for line in file.readlines():
-            item = json.loads(line)
-            res = search_item(item, "maflra", "user")
-            if res != None:
-                events.append(res)
-            res = search_item(item, "6K91vRPSfuUUIvd8xc82Hp", "playlist")
-            if res != None:
-                events.append(res)
-            res = search_item(item, "3JKfyBJZvr3hENnSokG7RQ", "track")
-            if res != None:
-                events.append(res)
-            break
-        return events
+        for (idx, line) in enumerate(file.readlines()):
+            yield (idx, json.loads(line))
+
+
+def s_items(items:list, l:list, id_, type_):
+    l1 = len(items)
+    for entry in l:
+        if type(entry) == dict:
+            it = search_item(entry, id_, type_)
+            if it != None:
+                items.append(it)
+            else:
+                pass
+    if items == []:
+        return None
+    else:
+        return (int(len(items)-l1), len(l))
 
 
 def search_item(item: "dict[str, str, str, list]", id_, type_):
-    if type_ == "playlist" or type_ == "user":
-        try:
-            if item["id"] == id_ and item["type"] == type_:
-                return item
-            elif type(item["data"]) == list:
-                items = []
-                for entry in item["data"]:
-                    if type(entry) == dict:
-                        it = search_item(entry, id_, type_)
-                        if it != None:
-                            items.append(it)
-                if items != []:
-                    return {
-                        "id": item["id"],
-                        "type": item["type"],
-                        "op": item["op"],
-                        "data": [
-                            {
-                                "id": entry["id"],
-                                "op":entry["op"],
-                                "type":entry["type"],
-                                "data":items
-                            }
-                        ]
-                    }
-
-                else:
-                    return None
+    if type_ == "user":
+        if item["id"] == id_ and item["type"] == type_:
+            return item
+        elif type(item["data"]) == list:
+            items = []
+            if s_items(items, item["data"], id_, type_)!=None:
+                return items
             else:
                 return None
-        except KeyError:
-            return None
-    elif type_ == "track":
-        if type(item) == dict:
-            for entry in item["data"]:
-                if entry["id"] == id_ and item["type"] == type_:
-                    return entry
-                try:
-                    if type(entry["data"]) == list:
-                        items = []
-                        for line in entry["data"]:
-                            if type(line) == dict:
-                                it = search_item(line, id_, type_)
-                                if it != None:
-                                    items.append(it)
-                        if items != []:
-                            return {
-                                "id": item["id"],
-                                "type": item["type"],
-                                "op": item["op"],
-                                "data": [
-                                    {
-                                        "id": entry["id"],
-                                        "op":entry["op"],
-                                        "type":entry["type"],
-                                        "data":[
-                                            {
-                                                "type": str(type_),
-                                                "timestamp": line["timestamp"],
-                                                "op":line["op"],
-                                                "data":items
-                                            }
-                                        ]
-                                    }
-                                ]
-                            }
+
+    elif type_ == "playlist":
+        if item["type"] == type_:
+            if item["id"] == id_:
+                return item
+            else:
+                return None
+        elif type(item["data"]) == list:
+            items = []
+            if s_items(items, item["data"], id_, type_)!=None:
+                n_dict = {}
+                for key in item.keys():
+                    if key == "data":
+                        n_dict["data"] = items
                     else:
-                        return None
-                except KeyError:
-                    return None
-                else:
-                    return None
-            return None
+                        n_dict[key] = item[key]
+                return n_dict
+            else:
+                return None
+
+    elif type_ == "track":
+        keys = item.keys()
+        if len(keys) == 2:
+            if item["id"] == id_:
+                return item
+            else:
+                return None
+        elif len(keys) == 4:
+            items = []
+            if s_items(items, item["data"], id_, type_)!=None:
+                n_dict = {}
+                for key in keys:
+                    if key == "data":
+                        n_dict["data"] = items
+                    else:
+                        n_dict[key] = item[key]
+                return n_dict
+            else:
+                return None
